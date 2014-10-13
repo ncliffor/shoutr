5db7e118fd17ad477dbd702b9dc9e61f5a862df2
Nick Clifford <ncliffor@gmail.com>
2014-10-13 11:04:01 -0400

    Users can create shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,5 +1,14 @@
 class ShoutsController < ApplicationController
+  before_action :require_login
+
   def index
     @shouts = Shout.all
+    @shout = Shout.new
+  end
+
+  def create
+    Shout.create(user_id: current_user.id, body: params[:shout][:body])
+
+    redirect_to shouts_path
   end
 end
