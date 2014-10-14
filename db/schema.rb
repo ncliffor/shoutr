1c4f3b6fbab4e43472c9d9490c309c81b077dc74
Nick Clifford <ncliffor@gmail.com>
2014-10-14 17:30:34 -0400

    Add Polymorphic migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141013175502) do
+ActiveRecord::Schema.define(version: 20141014210810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,12 +29,19 @@ ActiveRecord::Schema.define(version: 20141013175502) do
 
   create_table "shouts", force: true do |t|
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "subject_id",   null: false
+    t.string   "subject_type", null: false
+  end
+
+  add_index "shouts", ["user_id"], name: "index_shouts_on_user_id", using: :btree
+
+  create_table "text_subjects", force: true do |t|
     t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "shouts", ["user_id"], name: "index_shouts_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
