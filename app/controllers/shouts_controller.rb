142ee5d924067d02827510ba9fe23bfee9884e58
Nick Clifford <ncliffor@gmail.com>
2014-10-15 15:50:40 -0400

    Add pagination to shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -2,10 +2,10 @@ class ShoutsController < ApplicationController
   before_action :require_login
 
   def index
-    @timeline = current_user.timeline
+    @timeline = current_user.timeline.page(params[:page]).per(3)
     @text_subject = TextSubject.new
     @image_subject = ImageSubject.new
     @users_i_follow = current_user.followed_users
-    @users_i_do_not_follow = User.where.not(id: current_user.followed_user_ids)
+    # @users_i_do_not_follow = User.where.not(id: current_user.followed_user_ids)
   end
 end
