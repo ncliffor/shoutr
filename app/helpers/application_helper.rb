02945e341d58c32a23e7c624769768581704a124
Nick Clifford <ncliffor@gmail.com>
2014-10-16 16:45:59 -0400

    Link usernames in shouts to user pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,14 @@
 module ApplicationHelper
-   def bold(text)
-     username = /@\w+/.match(text)
-     bold_username = content_tag(:strong, username)
-     text.gsub(/@\w+/, bold_username) 
+   # def bold(text)
+   #   username = /@\w+/.match(text)
+   #   bold_username = content_tag(:strong, username)
+   #   text.gsub(/@\w+/, bold_username) 
+   # end
+
+   def link_mention(text)
+     text.gsub(/@\w+/) do |mention|
+       username = mention[1..-1]
+       link_to mention, user_path(username)
+     end
    end
 end
